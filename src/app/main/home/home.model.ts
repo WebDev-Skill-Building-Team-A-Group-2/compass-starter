841e3d11fd1923dd3c6054af31eec68af2674c76
karora9 <karora9@ucsc.edu>
2026-07-21 13:55:55 -0700

    feat: implement Quarterly Goals Item

--- src/app/main/home/home.model.ts
+++ src/app/main/home/home.model.ts
@@ -1,1 +1,9 @@
 // Add any extra data types you'll need here!
+import { QuarterlyGoal } from '../../core/store/quarterly-goal/quarterly-goal.model';
+import { Hashtag } from '../../core/store/hashtag/hashtag.model';
+
+export interface QuarterlyGoalData extends QuarterlyGoal {
+  hashtag: Hashtag;
+  weeklyGoalsTotal: number;
+  weeklyGoalsComplete: number;
+}
