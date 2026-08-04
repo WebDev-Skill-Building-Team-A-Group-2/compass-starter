ec05a5d8fa1201d8144042678d05c85ca797f902
Jay Li <jli921@ucsc.edu>
2026-08-04 12:58:06 -0700

    feat: implement Weekly Goals Item (#5)

--- src/app/main/home/home.model.ts
+++ src/app/main/home/home.model.ts
@@ -1,6 +1,10 @@
-// Add any extra data types you'll need here!
-import { QuarterlyGoal } from '../../core/store/quarterly-goal/quarterly-goal.model';
 import { Hashtag } from '../../core/store/hashtag/hashtag.model';
+import { WeeklyGoal } from '../../core/store/weekly-goal/weekly-goal.model';
+import { QuarterlyGoal } from '../../core/store/quarterly-goal/quarterly-goal.model';
+
+export interface WeeklyGoalData extends WeeklyGoal {
+  hashtag: Hashtag;
+}
 
 export interface QuarterlyGoalData extends QuarterlyGoal {
   hashtag: Hashtag;
--- src/app/main/home/weekly-goals/weekly-goals.component.ts
+++ src/app/main/home/weekly-goals/weekly-goals.component.ts
@@ -3,8 +3,13 @@ import { WeeklyGoalsAnimations } from './weekly-goals.animations';
 import { User } from 'src/app/core/store/user/user.model';
 import { AuthStore } from 'src/app/core/store/auth/auth.store';
 import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch-write.service';
-import { WeeklyGoalsModalComponent } from './weekly-goals-modal/weekly-goals-modal.component';
+import { WeeklyGoalsItemComponent } from './weekly-goals-item/weekly-goals-item.component';
+import { WeeklyGoal } from 'src/app/core/store/weekly-goal/weekly-goal.model';
+import { WeeklyGoalData } from '../home.model';
+import { Timestamp } from '@angular/fire/firestore';
 import { WeeklyGoalsHeaderComponent } from './weekly-goals-header/weekly-goals-header.component';
+import { WeeklyGoalsModalComponent } from './weekly-goals-modal/weekly-goals-modal.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-weekly-goals',
@@ -15,7 +20,8 @@ import { WeeklyGoalsHeaderComponent } from './weekly-goals-header/weekly-goals-h
   standalone: true,
   imports: [
     WeeklyGoalsHeaderComponent,
-    WeeklyGoalsModalComponent,
+    WeeklyGoalsItemComponent,
+    WeeklyGoalsModalComponent
   ],
 })
 export class WeeklyGoalsComponent implements OnInit {
@@ -31,12 +37,45 @@ export class WeeklyGoalsComponent implements OnInit {
   loading: WritableSignal<boolean> = signal(false);
 
   // --------------- COMPUTED DATA -----------------------
+    myTestGoal: WeeklyGoalData = {
+      __id: 'wg1',
+      __userId: 'test-user',
+      __quarterlyGoalId: 'qg1',
+      __hashtagId: 'ht1',
+      text: 'Finish Google Cover Letter',
+      completed: false,
+      order: 1,
+      _createdAt: Timestamp.now(),
+      _updatedAt: Timestamp.now(),
+      _deleted: false,
+      hashtag: {
+       __id: 'ht1',
+       __userId: 'test-user',
+       name: 'apply-internships',
+       color: '#EE8B72',
+       _createdAt: Timestamp.now(),
+       _updatedAt: Timestamp.now(),
+       _deleted: false,
+      },
+    };
 
   // --------------- EVENT HANDLING ----------------------
+  onGoalChecked(goal: WeeklyGoalData) {
+    this.snackBar.open(
+      goal.completed ? 'Marked goal as incomplete' : 'Marked goal as complete',
+      '',
+      {
+        duration: 3000,
+        verticalPosition: 'bottom',
+        horizontalPosition: 'center',
+      },
+    );
+  }
 
   // --------------- OTHER -------------------------------
 
   constructor(
+    private snackBar: MatSnackBar,
     private injector: Injector,
     @Inject(BATCH_WRITE_SERVICE) private batch: BatchWriteService,
   ) { }
