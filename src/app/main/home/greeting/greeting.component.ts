592ad29209f761d971783c1a70b4d8e5c7e3d5f7
Ethan <etvinh@ucsc.edu>
2026-08-06 22:53:41 +0200

    feat: implement Greeting

--- src/app/main/home/greeting/greeting.component.ts
+++ src/app/main/home/greeting/greeting.component.ts
@@ -3,6 +3,10 @@ import { GreetingAnimations } from './greeting.animations';
 import { User } from 'src/app/core/store/user/user.model';
 import { AuthStore } from 'src/app/core/store/auth/auth.store';
 import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch-write.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { interval, map } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-greeting',
@@ -27,6 +31,28 @@ export class GreetingComponent implements OnInit {
   loading: WritableSignal<boolean> = signal(false);
 
   // --------------- COMPUTED DATA -----------------------
+  time: Signal<Date> = toSignal(
+    interval(1000).pipe(
+      map(() => new Date())
+    ),
+    { initialValue: new Date() }
+  );
+
+  greeting: Signal<string> = computed(() => {
+    const currentHour = this.time().getHours();
+  
+    let greetingMessage: string;
+  
+    if (currentHour >= 5 && currentHour < 12) {
+      greetingMessage = 'Good morning';
+    } else if (currentHour < 18) {
+      greetingMessage = 'Good afternoon';
+    } else {
+      greetingMessage = 'Good evening';
+    }
+  
+    return greetingMessage;
+  });
 
   // --------------- EVENT HANDLING ----------------------
 
@@ -40,5 +66,6 @@ export class GreetingComponent implements OnInit {
   // --------------- LOAD AND CLEANUP --------------------
   
   ngOnInit(): void {
+    
   }
 }
