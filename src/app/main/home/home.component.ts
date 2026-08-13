c8c3a49a3f151f4ab079982cf2909d042a1ceb31
Jay Li <jli921@ucsc.edu>
2026-08-13 14:57:58 -0700

    feat: implement Long Term Goals Item (#8)

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -5,6 +5,7 @@ import { AuthStore } from 'src/app/core/store/auth/auth.store';
 import { BatchWriteService, BATCH_WRITE_SERVICE } from 'src/app/core/store/batch-write.service';
 import { NavbarComponent } from 'src/app/shared/navbar/navbar.component';
 import { WeeklyGoalsComponent } from './weekly-goals/weekly-goals.component';
+import { LongTermGoalsComponent } from './long-term-goals/long-term-goals.component'; 
 import { QuarterlyGoalsComponent } from './quarterly-goals/quarterly-goals.component';
 import { GreetingComponent } from './greeting/greeting.component';
 
@@ -15,9 +16,10 @@ import { GreetingComponent } from './greeting/greeting.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   animations: HomeAnimations,
-  imports: [WeeklyGoalsComponent,
+  imports: [
     NavbarComponent,
     WeeklyGoalsComponent,
+    LongTermGoalsComponent,
     QuarterlyGoalsComponent,
     GreetingComponent
   ]
